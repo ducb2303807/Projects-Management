0095182e6c230861cf143fb12ab3cd109addaa66
ducb2303807 <ducb2303807@student.ctu.edu.vn>
2026-02-11 22:34:52 +0700

    (WIP) feat: PluginLoader interface

--- Projects_Management_BE/src/main/java/com/group4/projects_management/controller/AuthController.java
+++ Projects_Management_BE/src/main/java/com/group4/projects_management/controller/AuthController.java
@@ -11,10 +11,13 @@ import com.group4.common.dto.UserRegistrationDTO;
 import com.group4.projects_management.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /** @pdOid 0607231e-fb06-4382-a69e-19c5a68f677d */
 @RestController
+@RequestMapping("/api/auth")
 public class AuthController {
    /** @pdRoleInfo migr=no name=UserService assc=association27 mult=1..1 */
    @Autowired
@@ -22,6 +25,7 @@ public class AuthController {
    
    /** @param request
     * @pdOid 91c91f2f-b3dc-4edd-a7ac-489cf42393ff */
+   @PostMapping("/login")
    public ResponseEntity<AuthResponse> login(LoginRequest request) {
       // TODO: implement
       return null;
@@ -29,6 +33,7 @@ public class AuthController {
    
    /** @param request
     * @pdOid 7b5656f9-ec7d-4f11-8ccd-1696ae8ef5e3 */
+   @PostMapping("/register")
    public ResponseEntity<UserDTO> register(UserRegistrationDTO request) {
       // TODO: implement
       return null;
--- Projects_Management_FE/src/main/java/com/group4/projects_management_fe/MainController.java
+++ Projects_Management_FE/src/main/java/com/group4/projects_management_fe/MainController.java
@@ -7,6 +7,10 @@ public class MainController {
     @FXML
     private Label welcomeText;
 
+    // mỏng hơn
+    // UI có gì đó -> gửi thông báo thay đổi
+    // Khi mà có yêu cầu thay đổi gì đó trên UI -> Controller tự nhận thông báo và đưa lên UI
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
