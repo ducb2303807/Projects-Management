84aa2aa0b0d9fe3090949bff9aafaef64453c509
lesyeuxdehunny <giahandangnguyen251205@gmail.com>
2026-03-01 13:48:12 +0700

    chores: update TaskResponseDTO's attributes

--- common/src/main/java/com/group4/common/dto/TaskResponseDTO.java
+++ common/src/main/java/com/group4/common/dto/TaskResponseDTO.java
@@ -25,7 +25,9 @@ public class TaskResponseDTO extends TaskBaseDTO {
    /** @pdOid e019add9-42da-4100-a916-ff41591b1018 */
    private java.lang.String priorityName;
    /** @pdOid c152bbcf-e2ec-414b-8544-cb5467e6d178 */
-   private LocalDateTime createAt;
+   private LocalDateTime createdAt;
+   /** @pdOid d3afb28f-2f74-4b61-8f77-a7d7605106c1 */
+   private LocalDateTime updatedAt;
    /** @pdOid 4aadc7a4-f347-42bc-b25f-b5a70ce7fc9b */
    private List<TaskAssigneeDTO> assignees;
 
