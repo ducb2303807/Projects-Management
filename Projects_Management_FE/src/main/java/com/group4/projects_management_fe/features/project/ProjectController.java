28763bde1dfc2afd1904f941b7289f8b0f6d5da8
nghib2303830 <nghib2303830@student.ctu.edu.vn>
2026-03-09 14:01:32 +0700

    feat: project view
    # Conflicts:
    #	Projects_Management_FE/src/main/resources/com/group4/projects_management_fe/features/assets/image/folder_v3.png
    #	Projects_Management_FE/src/main/resources/com/group4/projects_management_fe/features/project/ProjectsView.fxml

--- Projects_Management_FE/src/main/java/com/group4/projects_management_fe/features/project/ProjectController.java
+++ Projects_Management_FE/src/main/java/com/group4/projects_management_fe/features/project/ProjectController.java
@@ -1,4 +1,53 @@
 package com.group4.projects_management_fe.features.project;
 
-public class ProjectController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProjectController implements Initializable {
+
+    @FXML
+    private HBox recentCardsContainer;
+
+    @FXML
+    private FlowPane mainCardsContainer;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadRecentProjects();
+        loadMainProjects();
+    }
+
+    private void loadRecentProjects() {
+        try {
+            for (int i = 0; i < 6; i++) {
+                // Sửa lại đường dẫn nạp FXML cho đúng với cấu trúc thư mục của project bạn
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/group4/projects_management_fe/features/project/RecentProjectCard.fxml"));
+                Node recentCard = loader.load();
+                recentCardsContainer.getChildren().add(recentCard);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadMainProjects() {
+        try {
+            for (int i = 0; i < 5; i++) {
+                // Sửa lại đường dẫn nạp FXML cho đúng với cấu trúc thư mục của project bạn
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/group4/projects_management_fe/features/project/ProjectCard.fxml"));
+                Node projectCard = loader.load();
+                mainCardsContainer.getChildren().add(projectCard);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
