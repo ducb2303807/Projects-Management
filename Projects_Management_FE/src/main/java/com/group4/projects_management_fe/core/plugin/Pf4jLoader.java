609c3d21ad1d884cbd42897ece86fd25c2825374
ducb2303807 <ducb2303807@student.ctu.edu.vn>
2026-03-12 23:20:17 +0700

    feat: Hello World Plugin

--- Projects_Management_FE/src/main/java/com/group4/projects_management_fe/core/plugin/Pf4jLoader.java
+++ Projects_Management_FE/src/main/java/com/group4/projects_management_fe/core/plugin/Pf4jLoader.java
@@ -15,11 +15,12 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
-public class Pf4jLoader {
-    private PluginManager pluginManager;
+public class Pf4jLoader implements PluginLoader {
     private final Path officialPath = Paths.get("plugins/official");
     private final Path customPath = Paths.get("plugins/custom");
-    private HostContext hostContext;
+    private Thread loadThread;
+    private final PluginManager pluginManager;
+    private final HostContext hostContext;
 
     public Pf4jLoader(HostContext hostContext) {
         this.hostContext = hostContext;
@@ -27,6 +28,8 @@ public class Pf4jLoader {
         try {
             Files.createDirectories(officialPath);
             Files.createDirectories(customPath);
+            System.out.println("tạo thư mục plugin thành công");
+
         } catch (IOException e) {
             System.err.println("Không thể tạo thư mục plugin: " + e.getMessage());
         }
@@ -50,6 +53,7 @@ public class Pf4jLoader {
                 for (Plugin plugin : plugins) {
                     try {
                         plugin.init(hostContext);
+                        plugin.start();
 
                         Platform.runLater(() -> {
                             List<WidgetProvider> providers = plugin.getWidgetProviders();
@@ -63,13 +67,42 @@ public class Pf4jLoader {
                     } catch (Exception e) {
                         System.err.println("Lỗi khi nạp plugin " + plugin.getName() + ": " + e.getMessage());
                     }
+
                 }
                 return null;
             }
         };
 
-        Thread thread = new Thread(loadTask);
-        thread.setDaemon(true); // tắt thread khi đóng app
-        thread.start();
+        loadThread = new Thread(loadTask);
+        loadThread.setDaemon(true); // tắt thread khi đóng app
+        loadThread.start();
+    }
+
+    public void unloadPlugins() {
+        System.out.println("Đang tiến hành dừng các plugins...");
+
+        if (loadThread != null && loadThread.isAlive()) {
+            try {
+                loadThread.join(2000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        try {
+            List<Plugin> plugins = pluginManager.getExtensions(Plugin.class);
+            if (plugins != null) {
+                for (Plugin plugin : plugins) {
+                    plugin.stop();
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Lỗi khi gọi hàm stop() của nghiệp vụ plugin: " + e.getMessage());
+        }
+
+        // không cần stop vì thư viện đã làm điều đó
+        pluginManager.unloadPlugins();
+
+        System.out.println("Đã dọn dẹp plugins thành công.");
     }
 }
