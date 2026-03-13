88feeaa1be16edfaffdbf85cf9fe33bb6b2c979f
ducb2303807 <ducb2303807@student.ctu.edu.vn>
2026-03-13 20:49:58 +0700

    feat: Stage manager

---
+++ Projects_Management_FE/src/main/java/com/group4/projects_management_fe/core/navigation/AppStageManager.java
@@ -0,0 +1,25 @@
+﻿package com.group4.projects_management_fe.core.navigation;
+
+import javafx.stage.Stage;
+
+public class AppStageManager {
+    private static AppStageManager instance;
+    private Stage stage;
+
+    private AppStageManager() {}
+
+    public synchronized static AppStageManager getInstance() {
+        if (instance == null) {
+            instance = new AppStageManager();
+        }
+        return instance;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+}
